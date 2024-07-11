33924070fe89b76f1de9ca83b21cb610dd35030b
loweek.eth <49698528+Locinone@users.noreply.github.com>
2024-07-11 22:50:50 +0200

    Revert "ajout de l'importation JSON des jobs avec l'URI http://localhost:5000…"
    This reverts commit 485ff95696bcfaeb175426fbc6f2708c44cccaf5.

--- backend/utils/catchError.js
+++
@@ -1,9 +0,0 @@
-// fonction qui permet d'afficher exactement l'erreur en question
-
-export default function afficheError(err, res) {
-  console.log(err);
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-  });
-}
--- backend/utils/hashCode.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt";
-const salt = 10;
-export const hashPassword = (newPassword) => {
-  const NewPasswordHash = bcrypt.hashSync(newPassword, salt);
-  return NewPasswordHash;
-};
